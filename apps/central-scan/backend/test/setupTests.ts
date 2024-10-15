2a890f263eb1bc8a4cdf64c94f3252014bc8a5f5
Kofi <qlibrium@gmail.com>
2024-10-15 11:34:11 -0500

    Prune config files in favour of consolidating (#3)
    Removing config files in subdirectories that will later share a common
    config:
    - vite.config
    - playwright.config
    - jest.config/setupTests
    
    Also removing unused seal SVGs, since we've moved to embedded SVGs in
    election definitions.

--- apps/central-scan/backend/test/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import { cleanupCachedBrowser } from '@vx/libs/printing/src';
-
-afterAll(async () => {
-  await cleanupCachedBrowser();
-});
--- libs/bmd-ballot-fixtures/test/setupTests.ts
+++
@@ -1,8 +0,0 @@
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
-import { cleanupCachedBrowser } from '@vx/libs/printing/src';
-
-afterAll(async () => {
-  await cleanupCachedBrowser();
-});
-
-expect.extend({ toMatchImageSnapshot });
