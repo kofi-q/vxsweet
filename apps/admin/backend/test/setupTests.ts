86ea80db122c43856fa5e3bc9133303b980b0f3b
Kofi <kofi@voting.works>
2024-10-15 11:28:56 -0500

    Prune config files in favour of consolidating

--- apps/admin/backend/test/setupTests.ts
+++
@@ -1,7 +0,0 @@
-import { cleanupCachedBrowser } from '@vx/libs/printing/src';
-import { cleanupTestSuiteTmpFiles } from './cleanup';
-
-afterAll(async () => {
-  cleanupTestSuiteTmpFiles();
-  await cleanupCachedBrowser();
-});
