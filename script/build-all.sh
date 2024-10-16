12778ddf6568a750d6c3c00db51dcf072413e528
Kofi <kofi@voting.works>
2024-10-16 15:41:45 -0500

    Remove rustc path hack from CI scripts - no longer needed

--- script/build-all.sh
+++ script/build-all.sh
@@ -2,16 +2,6 @@
 
 set -euo pipefail
 
-if [[ -z $(which rustc) ]]; then
-  # Transitive dependency `libudev-sys` invokes `rustc` directly on the command
-  # line in its build script - this makes sure it uses the bazel-provided
-  # version.
-
-  # shellcheck disable=SC2147
-  RUSTC_PATH="$(bazel info output_base)/external/rules_rust~~rust_host_tools~rust_host_tools/bin/rustc"
-  ln -sf "${RUSTC_PATH}" /usr/local/bin/rustc
-fi
-
 OPTIONAL_CI_FLAGS=""
 if [[ -n "${CI-}" ]]; then
   # See .bazel/ci.bazelrc for related Bazel flags:
