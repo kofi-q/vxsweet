163412f645f8d6f47de60998792b0eecef74a89b
Kofi <kofi@voting.works>
2024-10-19 11:35:08 -0500

    Enable libs/res-to-ts tests

--- libs/res-to-ts/src/convert.test.ts
+++ libs/res-to-ts/src/convert.test.ts
@@ -4,13 +4,15 @@ import tmp from 'tmp';
 import { convert } from './convert';
 
 test('creating a resource for a directory', async () => {
-  fs.mkdirSync('/tmp/data/electionFixture/cvr-files', { recursive: true });
+  fs.mkdirSync(`${tmp.tmpdir}/data/electionFixture/cvr-files`, {
+    recursive: true,
+  });
   const tmpDirName = tmp.dirSync({
-    template: '/tmp/data/electionFixture/cvr-files/resource-XXXXXX',
+    template: `${tmp.tmpdir}/data/electionFixture/cvr-files/resource-XXXXXX`,
   }).name;
   const result = await convert({
     path: tmpDirName,
-    tsPath: '/tmp/src/data/electionFixture/cvr-files/resource.ts',
+    tsPath: `${tmp.tmpdir}/src/data/electionFixture/cvr-files/resource.ts`,
     mimeType: 'application/octet-stream',
   });
   const tmpDirBaseName = basename(tmpDirName);
