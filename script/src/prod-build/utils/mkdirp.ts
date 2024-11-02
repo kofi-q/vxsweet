3840109bc5aee6b76553f72aed465901f4db7ea0
Kofi <kofi@voting.works>
2024-11-02 13:25:52 -0500

    Remove unused filaed

--- script/src/prod-build/utils/mkdirp.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'node:fs';
-
-export function mkdirp(path: string): void {
-  fs.mkdirSync(path, { recursive: true });
-}
--- script/src/prod-build/utils/relative_path.ts
+++
@@ -1,9 +0,0 @@
-import { isAbsolute, relative } from 'node:path';
-
-export function relativePath(path: string, { from }: { from: string }): string {
-  if (isAbsolute(path)) {
-    return relative(from, path);
-  } else {
-    return path;
-  }
-}
