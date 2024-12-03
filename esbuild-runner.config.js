3a2f11be125ee8f13813ce006f974d14fa1bac4a
Kofi <kofi@voting.works>
2024-12-03 16:10:43 -0600

    Remove unused esbuild-runner artifacts/deps

--- esbuild-runner.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable vx/gts-module-snake-case */
-module.exports = {
-  type: 'transform',
-  esbuild: {
-    // Any esbuild build or transform options go here
-    target: 'es2019',
-  },
-};
